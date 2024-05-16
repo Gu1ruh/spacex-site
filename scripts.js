9f1fd97e1f43a354e6e31c03e22e96945a091938
HP PROBOOK 640 G2 <kiokopraxides@gmail.com>
2024-05-16 08:09:29 +0300

    my first commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+// Function to show the next slide
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+// Function to show the previous slide
+function prevSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
